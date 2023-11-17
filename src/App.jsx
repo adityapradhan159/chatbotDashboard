b624c8b338f54fb59a39e14446fb4bd68dfdc8bd
adityapradhan159 <adityapradhan@tudotechnologies.com>
2023-11-17 10:33:21 +0530

    latest changes

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,30 @@ import 'reactflow/dist/style.css';
 import FlowSettings from "./Pages/FlowSettings/FlowSettings";
 
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAIPSlIghpPxdHJmT6zgHMCmc4dzZpbR5o",
+  authDomain: "chatbotdashboardv2.firebaseapp.com",
+  projectId: "chatbotdashboardv2",
+  storageBucket: "chatbotdashboardv2.appspot.com",
+  messagingSenderId: "1038335016642",
+  appId: "1:1038335016642:web:6d4148261512c374f8c55b",
+  measurementId: "G-P58B5DZY7F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 function App() {
   const [messages, setMessages] = useState([]);
   const [AllChats, setAllChats] = useState([]);
--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -1,32 +1,81 @@
-/* eslint-disable no-unused-vars */
-import React, { Component } from "react";
+// /* eslint-disable no-unused-vars */
+// import React, { Component } from "react";
+// import "./userProfile.css";
+// import {AiFillCaretDown} from 'react-icons/ai'
+
+
+
+// export default class UserProfile extends Component {
+
+
+//   toggleInfo = (e) => {
+//     e.target.parentNode.classList.toggle("open");
+//   };
+
+
+
+
+//   render() {
+//     return (
+//       <div className="main__userprofile">
+//         <div className="profile__card user__profile__image">
+//           <div className="profile__image">
+//             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU" />
+//           </div>
+//           <h4>Fernando Faucho</h4>
+//           <p>CEO & Founder at Highly Inc</p>
+//         </div>
+//         <div className="profile__card">
+//           <div className="card__header" onClick={()=>c("/settings")}>
+//             <h4>Flow Setting</h4>
+//             {/* <AiFillCaretDown/> */}
+//           </div>
+//           <div className="card__content">
+//             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+//             ultrices urna a imperdiet egestas. Donec in magna quis ligula
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+
+import React, { useState } from "react";
 import "./userProfile.css";
-import {AiFillCaretDown} from 'react-icons/ai'
-export default class UserProfile extends Component {
-  toggleInfo = (e) => {
-    e.target.parentNode.classList.toggle("open");
+import { AiFillCaretDown } from 'react-icons/ai';
+import { useNavigate } from "react-router-dom";
+
+const UserProfile = () => {
+
+  const navigate = useNavigate()
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleInfo = () => {
+    setIsOpen(!isOpen);
   };
-  render() {
-    return (
-      <div className="main__userprofile">
-        <div className="profile__card user__profile__image">
-          <div className="profile__image">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU" />
-          </div>
-          <h4>Fernando Faucho</h4>
-          <p>CEO & Founder at Highly Inc</p>
+
+  return (
+    <div className={`main__userprofile ${isOpen ? "open" : ""}`}>
+      <div className="profile__card user__profile__image">
+        <div className="profile__image">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTA78Na63ws7B7EAWYgTr9BxhX_Z8oLa1nvOA&usqp=CAU" alt="User Profile" />
         </div>
-        <div className="profile__card">
-          <div className="card__header" onClick={this.toggleInfo}>
-            <h4>Flow Setting</h4>
-            <AiFillCaretDown/>
-          </div>
-          <div className="card__content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-            ultrices urna a imperdiet egestas. Donec in magna quis ligula
-          </div>
+        <h4>Fernando Faucho</h4>
+        <p>CEO & Founder at Highly Inc</p>
+      </div>
+      <div className="profile__card">
+        <div className="card__header" onClick={() => navigate("/settings")}>
+          <h4>Flow Setting</h4>
+          {/* <AiFillCaretDown/> */}
+        </div>
+        <div className="card__content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          ultrices urna a imperdiet egestas. Donec in magna quis ligula
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default UserProfile;
